14efcd272554e5a94734dc2768b2bd028b33da58
Mauricio Rodrigues <mauricio.log98@hotmail.com>
2022-05-28 21:46:02 -0300

    Fix bugs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;800&display=swap"
                           rel="stylesheet"/>
+                    <title>Meu Carrinho</title>
                 </Head>
                 <body>
                 <Main/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,61 @@
-import React from 'react'
-import styles from './home.module.scss'
-import jsonMinor from '../services/utils'
+import React, {useState} from 'react'
+import {Container} from '../components/Container'
+import styles from './styles.module.scss'
+import {Header} from "../components/Header";
+import json from '../services/utils'
+import {Product} from "../components/Product";
+import {Footer} from "../components/Footer";
+import {useShipping} from "../hooks/shipping";
 
-export default function Home() {
+export default function MyKart() {
+
+    const {
+        active
+    } = useShipping()
 
     return (
-        <>
-            <div className={styles.container}>
+        <div className={styles.container}>
+            <Container theme={"white"}>
 
-            </div>
-        </>
+                <div className={styles.header}>
+                    <Header title={"Meu carrinho"}/>
+                </div>
+
+                <div className={styles.list}>
+                    <ul> {!active ? <>
+                            {json.json.items.map((product) => {
+                                return (
+                                    <Product
+                                        key={product.id}
+                                        data={product}
+                                    />
+                                )
+                            })
+                            }
+                        </> :
+                        <>{
+                            json.jsonBigger.items.map((product) => {
+                                return (
+                                    <Product
+                                        key={product.id}
+                                        data={product}
+                                    />
+                                )
+                            })
+                        }
+                        </>
+                    }
+
+
+                    </ul>
+                </div>
+
+                <div className={styles.footer}>
+                    <Footer/>
+                </div>
+
+            </Container>
+
+        </div>
     )
 }
